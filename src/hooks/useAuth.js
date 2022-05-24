c81e38ac14573332872db2578bf6dca00e545892
davidjorge <davidjorge126@gmail.com>
2022-05-24 14:30:57 -0500

    :bricks: [MOD]
    API token addition

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,12 @@
 import React, { useState, useContext, createContext } from 'react';
 import Cookie from 'js-cookie';
-import axios from 'axios';
-import endPoints from '@services/api/';
+import Axios from 'axios';
+import endPoints from '@services/api/index';
 
 const AuthContext = createContext();
 
 export function ProviderAuth({ children }) {
-  const auth = useProviderAuth();
+  const auth = useProvideAuth();
 
   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 }
@@ -15,19 +15,24 @@ export const useAuth = () => {
   return useContext(AuthContext);
 };
 
-function useProviderAuth() {
+function useProvideAuth() {
   const [user, setUser] = useState(null);
 
   const signIn = async (email, password) => {
     const options = {
       headers: {
+        'access-control-allow-origin': '*',
         accept: '*/*',
         'Content-Type': 'application/json',
       },
     };
 
-    const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
-    console.log(access_token);
+    // Axios get method error, requested service error
+    const { data: access_token } = await Axios.post(endPoints.auth.login, { email, password }, options);
+    // Token saved on local storage
+    if (access_token) {
+      Cookie.set('token', access_token.access_token, { expires: 5 });
+    }
   };
 
   return { user, signIn };
